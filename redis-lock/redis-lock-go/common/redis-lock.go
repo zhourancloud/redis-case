b1b20032accaceadac051fa04cdaad228b4f0602
zhourancloud <1787706777@qq.com>
2022-01-18 23:35:43 +0800

    redis分布式锁

--- redis-lock/redis-lock-go/common/redis-lock.go
+++ redis-lock/redis-lock-go/common/redis-lock.go
@@ -1,6 +1,9 @@
 package main
 
 //https://www.cnblogs.com/liyutian/p/10050320.html
+//https://blog.csdn.net/zjcjava/article/details/112817499?spm=1001.2101.3001.6650.3&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-3.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-3.pc_relevant_aa&utm_relevant_index=6
+//https://mp.weixin.qq.com/s?__biz=Mzg2ODU1MTI0OA==&mid=2247484739&idx=1&sn=6fb754a4e88a04d9c14af17379be7eb2&chksm=ceabda7cf9dc536a7c0d84a474f8a79c1a7168c5f456c37133aa236d081f9cf785f3fbddc989&mpshare=1&scene=23&srcid=0115aKY9n5RCFt8bXp9OgiaQ&sharer_sharetime=1642248824024&sharer_shareid=e310c4d733aaa12c88d17d19e93aab6e#rd
+
 import (
 	"fmt"
 	"strconv"
@@ -36,8 +39,8 @@ func newRedisLock(redisdb redis.Client) *RedisLock {
 	}
 }
 
-// 加锁函数
-func (lock *RedisLock) AcquireLock(key string, expire int) bool {
+// 尝试加锁函数
+func (lock *RedisLock) try_lock(key string, expire int) bool {
 	lock_script := redis.NewScript(lock.lock_lua)
 	if lock_script == nil {
 		fmt.Println("lock_script :", lock_script)
@@ -45,8 +48,8 @@ func (lock *RedisLock) AcquireLock(key string, expire int) bool {
 	}
 	expire_time := strconv.Itoa(expire * 1000)
 	n, err := lock_script.Run(&lock.redisdb, []string{key}, []string{lock.uid, expire_time}).Result()
-	fmt.Println("result: ", n, err)
-
+	fmt.Println("lock result n:", n)
+	fmt.Println("lock result err:", err)
 	return true
 }
 
@@ -65,8 +68,12 @@ func main() {
 	}
 
 	redis_lock1 := newRedisLock(*redis_client)
-	result := redis_lock1.AcquireLock("order", 15)
-	fmt.Print("result: ", result)
-	result = redis_lock1.AcquireLock("order", 15)
-	fmt.Print("result: ", result)
+	redis_lock2 := newRedisLock(*redis_client)
+
+	redis_lock1.try_lock("order", 15)
+	//fmt.Println("1111111122222")
+	redis_lock1.try_lock("order", 15)
+	//fmt.Println("11111111111")
+	redis_lock2.try_lock("order", 15)
+
 }
